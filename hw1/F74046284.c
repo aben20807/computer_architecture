9fe79f78aaecf8a8616ef102f27149e0149eed13
aben20807 <aben20807@gmail.com>
2018-10-07 00:39:25 +0800

    Add enum for op an funct

--- hw1/F74046284.c
+++ hw1/F74046284.c
@@ -1,6 +1,73 @@
 #include <stdio.h>
 #include <string.h>
-#include <ctype.h>
+
+typedef enum _OP {
+    OP_R        = 0,
+    OP_BLTZ     = 1,
+    OP_J        = 2,
+    OP_Jal      = 3,
+    OP_BEQ      = 4,
+    OP_BNE      = 5,
+    OP_BLEZ     = 6,
+    OP_BGTZ     = 7,
+    OP_ADDI     = 8,
+    OP_ADDIU    = 9,
+    OP_SLTI     = 10,
+    OP_SLTIU    = 11,
+    OP_ANDI     = 12,
+    OP_ORI      = 13,
+    OP_XORI     = 14,
+    OP_LUI      = 15,
+    OP_TLB      = 16,
+    OP_FIPT     = 17,
+    OP_LB       = 32,
+    OP_LH       = 33,
+    OP_LWI      = 34,
+    OP_LW       = 35,
+    OP_LBU      = 36,
+    OP_LHU      = 37,
+    OP_LWR      = 38,
+    OP_SB       = 40,
+    OP_SH       = 41,
+    OP_SWI      = 42,
+    OP_SW       = 43,
+    OP_SWR      = 46,
+    OP_LL       = 48,
+    OP_LWC1     = 49,
+    OP_SC       = 56,
+    OP_SWC1     = 67,
+} op_t;
+
+typedef enum _FUNCT {
+    F_SLL       = 0,
+    F_SRL       = 2,
+    F_SRA       = 3,
+    F_SLLV      = 4,
+    F_SRLV      = 6,
+    F_SRAV      = 7,
+    F_JR        = 8,
+    F_JALR      = 9,
+    F_SYSCALL   = 12,
+    F_BREAK     = 13,
+    F_MFHI      = 16,
+    F_MTHI      = 17,
+    F_MFLO      = 18,
+    F_MTLO      = 19,
+    F_MULT      = 24,
+    F_MULTU     = 25,
+    F_DIV       = 26,
+    F_DIVU      = 27,
+    F_ADD       = 32,
+    F_ADDU      = 33,
+    F_SUB       = 34,
+    F_SUBU      = 35,
+    F_AND       = 36,
+    F_OR        = 37,
+    F_XOR       = 38,
+    F_NOR       = 39,
+    F_SLT       = 42,
+    F_SLTU      = 43,
+} funct_t;
 
 static const char *table[] = {
     "0000", "0001", "0010", "0011",
@@ -9,7 +76,8 @@ static const char *table[] = {
     "1100", "1101", "1110", "1111",
 };
 
-int c2i(char c);
+int h2d(char c);
+int b2d(char *s, int start, int end);
 
 int main(int argc, char *argv[])
 {
@@ -21,19 +89,38 @@ int main(int argc, char *argv[])
     while (!feof(fin)) {
         if (fgets(line, 20, fin) != NULL) {
             sprintf(bin, "%s%s%s%s%s%s%s%s",
-                    table[c2i(line[0])],
-                    table[c2i(line[1])],
-                    table[c2i(line[3])],
-                    table[c2i(line[4])],
-                    table[c2i(line[6])],
-                    table[c2i(line[7])],
-                    table[c2i(line[9])],
-                    table[c2i(line[10])]);
+                    table[h2d(line[0])],
+                    table[h2d(line[1])],
+                    table[h2d(line[3])],
+                    table[h2d(line[4])],
+                    table[h2d(line[6])],
+                    table[h2d(line[7])],
+                    table[h2d(line[9])],
+                    table[h2d(line[10])]);
             printf("%s\n", bin);
         }
         if (strcmp(bin, "00000000000000000000000000000000") == 0) {
-            printf("nop");
+            printf("nop\n");
+            goto nxt;
         }
+        int op = b2d(bin, 0, 6);
+        switch (op) {
+            case OP_R: /* R-format */
+                printf("R\n");
+                break;
+            case 1:
+                break;
+            case OP_J:
+                printf("J\n");
+                break;
+        }
+        // printf("%d ", b2d(bin, 0, 6));
+        // printf("%d ", b2d(bin, 6, 11));
+        // printf("%d ", b2d(bin, 11, 16));
+        // printf("%d ", b2d(bin, 16, 21));
+        // printf("%d ", b2d(bin, 21, 26));
+        // printf("%d\n", b2d(bin, 26, 32));
+nxt:;
     }
 
     fclose(fout);
@@ -41,10 +128,20 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-int c2i(char c) {
-    if (isdigit(c))
+int h2d(char c)
+{
+    if (c >= '0' && c <= '9')
         return c - '0';
-    if (isalpha(c))
+    if (c >= 'A' && c <= 'F')
         return c - 'A' + 10;
     return -1;
+}
+
+int b2d(char *s, int start, int end)
+{
+    int ret = 0;
+    for (int i = end - 1, e = 1; i >= start; i--, e *= 2) {
+        ret += ((s[i] - '0') * e);
+    }
+    return ret;
 }
